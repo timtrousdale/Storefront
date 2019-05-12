e3eb5edf53f5b28083a5aa18d8027c9a89a6a22a
TNT <timrousdale@gmail.com>
2019-05-12 00:46:41 -0600

    Updates

---
+++ precache-manifest.d21c17bf3e21dc5b11ef1026354ea3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761dfb54bf20fa442d55fc59d5e6d97c",
+    "url": "/Storefront/index.html"
+  },
+  {
+    "revision": "e5ae043576ca4823fb37",
+    "url": "/Storefront/static/css/main.3b412d55.chunk.css"
+  },
+  {
+    "revision": "6b8eca3d4d731a522f5a",
+    "url": "/Storefront/static/js/2.0c183e49.chunk.js"
+  },
+  {
+    "revision": "e5ae043576ca4823fb37",
+    "url": "/Storefront/static/js/main.eda42f53.chunk.js"
+  },
+  {
+    "revision": "396ec1dba6f326ff8f2a",
+    "url": "/Storefront/static/js/runtime~main.86375a5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f56d5c8d4edf8fc41f0a4a67880ec9a.js"
+  "/Storefront/precache-manifest.d21c17bf3e21dc5b11ef1026354ea3a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Storefront/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
